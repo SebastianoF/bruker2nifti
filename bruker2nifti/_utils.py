BUG float 32 / 64 in the slope corrector precision issue solved

bruker2nifti/_utils.py
@@ -241,7 +241,7 @@ def eliminates_consecutive_duplicates(input_list):
     return output_list
 
 
-def slope_corrector(data, slope, num_initial_dir_to_skip=None, dtype=np.float32):
+def slope_corrector(data, slope, num_initial_dir_to_skip=None, dtype=np.float64):
 
     # todo: need careful refactoring after meeting new cases
 
@@ -254,18 +254,21 @@ def slope_corrector(data, slope, num_initial_dir_to_skip=None, dtype=np.float32)
         slope = slope[num_initial_dir_to_skip:]
         data = data[..., num_initial_dir_to_skip:]
 
+    # correct for possible consecutive duplicates
     if not (isinstance(slope, int) or isinstance(slope, float)):
-        if len(slope.shape) == 1:
-            if not slope.size == data.shape[-1] or not slope.size == data.shape[-1]:
+        if slope.ndim == 1:
+            if not slope.size == data.shape[-1] and not slope.size == data.shape[-2]:
                 slope = np.array(eliminates_consecutive_duplicates(list(slope)), dtype=np.float64)
-                if not slope.size == data.shape[-1] or not slope.size == data.shape[-1]:
-                    raise IOError
+                if not slope.size == data.shape[-1] and not slope.size == data.shape[-2]:
+                    msg = 'Slope shape {0} and data shape {1} appears to be not compatible'.format(slope.shape, data.shape)
+                    raise IOError(msg)
 
     if isinstance(slope, int) or isinstance(slope, float):
-        # scalar times 3d array
+        # scalar times nd array
         data *= slope
 
     elif slope.size == 1:
+        # scalar embedded in a singleton times nd array
         data *= slope[0]
 
     elif len(data.shape) == 3 and len(slope.shape) == 1:
@@ -277,7 +280,7 @@ def slope_corrector(data, slope, num_initial_dir_to_skip=None, dtype=np.float32)
             raise IOError('Shape of the 2d image and slope dimensions are not consistent')
 
     elif len(data.shape) == 4 and len(slope.shape) == 1 and slope.shape[0] == data.shape[2]:
-
+        # each slice of the 4d image, taken from the third dimension, is multiplied by each element of the slope.
         if slope.size == data.shape[2]:
             for t in range(data.shape[3]):
                 for k in range(slope.size):
@@ -286,7 +289,7 @@ def slope_corrector(data, slope, num_initial_dir_to_skip=None, dtype=np.float32)
             raise IOError('If you are here, your case cannot be converted. Debug from here to include your case.')
 
     elif len(data.shape) == 5 and len(slope.shape) == 1 and slope.shape[0] == data.shape[3]:
-
+        # each slice of the 5d image, taken from the fourth dimension, is multiplied by each element of the slope.
         if slope.size == data.shape[3]:
             for t in range(data.shape[4]):
                 for k in range(slope.size):
@@ -295,11 +298,13 @@ def slope_corrector(data, slope, num_initial_dir_to_skip=None, dtype=np.float32)
             raise IOError('If you are here, your case cannot be converted. Debug from here to include your case.')
 
     else:
-        if slope.size == data.shape[3]:
-            for t in range(data.shape[3]):
+        # each slice of the nd image, taken from the last dimension, is multiplied by each element of the slope.
+        if slope.size == data.shape[-1]:
+            for t in range(data.shape[-1]):
                 data[..., t] = data[..., t] * slope[t]
         else:
-            raise IOError('Shape of the 3d image and slope dimensions are not consistent')
+            msg = 'Slope shape {0} and data shape {1} appears to be not compatible'.format(slope.shape, data.shape)
+            raise IOError(msg)
 
     return data
 

test/test_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import nibabel as nib
 
 from nose.tools import assert_equal, assert_true, assert_almost_equals, assert_raises
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_array_almost_equal
 
 from bruker2nifti._utils import indians_file_parser, normalise_b_vect, slope_corrector, \
     eliminate_consecutive_duplicates, compute_resolution_from_visu_pars, compute_affine_from_visu_pars, \
@@ -92,8 +92,8 @@ def test_slope_corrector_int_float_slope():
     sl1 = 5
     sl2 = 5.3
 
-    out_data1 = slope_corrector(in_data, sl1)
-    out_data2 = slope_corrector(in_data, sl2)
+    out_data1 = slope_corrector(in_data, sl1, dtype=np.float64)
+    out_data2 = slope_corrector(in_data, sl2, dtype=np.float64)
 
     assert_equal(out_data1.dtype, np.float64)
     assert_equal(out_data2.dtype, np.float64)
@@ -106,7 +106,7 @@ def test_slope_corrector_slice_wise_slope_3d():
     in_data = np.random.normal(5, 10, [4, 5, 3])
     sl = np.random.normal(5, 10, 3)
 
-    out_data = slope_corrector(in_data, sl)
+    out_data = slope_corrector(in_data, sl, dtype=np.float64)
 
     for k in range(3):
         assert_array_equal(out_data[..., k], in_data[..., k]*sl[k])
@@ -125,12 +125,11 @@ def test_slope_corrector_slice_wise_slope_4d():
 
     in_data = np.random.normal(5, 10, [2, 3, 4, 5])
     sl = np.random.normal(5, 10, 4)
-    out_data = slope_corrector(in_data, sl)
+    out_data = slope_corrector(in_data, sl, dtype=np.float64)
 
     for t in range(5):
         for k in range(4):
             assert_array_equal(out_data[..., k, t], in_data[..., k, t] * sl[k])
-
 
 # def test_slope_corrector_slice_wise_slope_4d_fail():
 #
@@ -145,7 +144,7 @@ def test_slope_corrector_slice_wise_slope_5d():
 
     in_data = np.random.normal(5, 10, [2, 3, 4, 5, 6])
     sl = np.random.normal(5, 10, 5)
-    out_data = slope_corrector(in_data, sl)
+    out_data = slope_corrector(in_data, sl, dtype=np.float64)
 
     for t in range(6):
         for k in range(5):
@@ -155,10 +154,12 @@ def test_slope_corrector_slice_wise_slope_5d():
 def test_slope_corrector_slice_wise_slope_5d_fail():
 
     in_data = np.random.normal(5, 10, [2, 3, 4, 5, 6])
-    sl = np.random.normal(5, 10, 6)
+    sl = np.random.normal(5, 10, 7)
 
     with assert_raises(IOError):
         slope_corrector(in_data, sl)
+
+test_slope_corrector_slice_wise_slope_5d_fail()
 
 
 # -- TEST nifti affine matrix utils --