5e3fc3928a03a9f0fd33a7e8dccfe716d235fe2c
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2017-07-24 09:28:00 +0100

    __init__.py updated

--- bruker2nifti/__init__.py
+++ bruker2nifti/__init__.py
@@ -1,1 +1,4 @@
-__all__ = ['_cores', '_getters', '_utils', 'scan_converter', 'study_converter']
+__author__     = "SebastianoF"
+__licence__    = "MIT"
+__repository__ = "https://github.com/SebastianoF/bruker2nifti"
+__all__ = ['_cores', '_getters', '_utils', 'converter', 'open_GUI']
