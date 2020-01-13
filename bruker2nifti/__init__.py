4fbac970e9125f3e53dd5e31d63b971ad739a6d2
SebastianoF <sebastiano.ferraris@gmail.com>
2020-01-13 21:11:10 +0000

    new release 1.0.4

--- bruker2nifti/__init__.py
+++ bruker2nifti/__init__.py
@@ -5,7 +5,7 @@ from subprocess import check_output
 __author__ = "Sebastiano Ferraris UCL"
 __licence__ = "MIT"
 __repository__ = "https://github.com/SebastianoF/bruker2nifti"
-__all__ = ["_cores", "_getters", "_utils", "converter", "open_GUI"]
+__all__ = ["_cores", "_getters", "_metadata", "_utils", "converter"]
 
 # here = os.path.abspath(os.path.dirname(__file__))
 # git_dir = os.path.dirname(here)
@@ -16,4 +16,4 @@ __all__ = ["_cores", "_getters", "_utils", "converter", "open_GUI"]
 
 # Exclude the 'v' for PEP440 conformity, see
 # https://www.python.org/dev/peps/pep-0440/#public-version-identifiers
-__version__ = "1.0.3"  # version_buf[1:].decode("utf-8")
+__version__ = "1.0.4"  # version_buf[1:].decode("utf-8")
